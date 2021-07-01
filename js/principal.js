b0690e458fc561f3a3a3dda5953e6772f02c84a8
Lorena C Silva <loricristinesilva@gmail.com>
2021-07-01 14:05:42 -0300

    Pega e envia dados
    - CreateElement = cria qualquer elemento HTML
    - Utilização do name de cada input para trazer o value
    - Colocar um elemento dentro do outro

--- js/principal.js
+++ js/principal.js
@@ -43,6 +43,36 @@ if (alturaValida && pesoValido){
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
 botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault(); //previne os comportamentos padrões dos usuários do browser
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr"); //passa o nome da tag que queremos criar
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = imc;
+
+    pacienteTr.appendChild(nomeTd); //Coloque como filho
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd); 
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    
+    tabela.appendChild(pacienteTr);
 })
 
 /*function mostraMensagem(){ 
