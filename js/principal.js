5ade5434d1d37b961395b494c2019af84d40ba4d
Lorena C Silva <loricristinesilva@gmail.com>
2021-07-01 14:00:17 -0300

    tirando o que segura o evento

--- js/principal.js
+++ js/principal.js
@@ -43,8 +43,6 @@ if (alturaValida && pesoValido){
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
 botaoAdicionar.addEventListener("click", function(event){
-    event.preventDefault();
-    console.log("olha só, posso chamar uma função anônima");
 })
 
 /*function mostraMensagem(){ 
